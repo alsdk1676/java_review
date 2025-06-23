e49d07e7a8c6a7bc36a67adfcb87354dd9820fee
alsdk1676 <alsdk6761@gmail.com>
2025-06-23 23:50:35 +0900

    java review 0623

---
+++ casting/src/castingTask2/MagicItem.java
@@ -0,0 +1,30 @@
+package castingTask2;
+
+public class MagicItem {
+//	공통 속성: name, grade, mana
+//	메서드: apply() → 기본 출력: "아이템을 사용했습니다"
+	String name;
+	int grade;
+	int mana;
+	
+	static {
+		System.out.println("아이템을 사용했습니다.");
+	}
+	
+	public MagicItem() {;}
+	
+
+	public MagicItem(String name, int grade, int mana) {
+		this.name = name;
+		this.grade = grade;
+		this.mana = mana;
+	}
+	
+	public void apply() {
+		System.out.println("아이템을 사용했습니다.");
+	}
+	
+	
+	
+
+}
---
+++ casting/src/castingTask2/Potion.java
@@ -0,0 +1,15 @@
+package castingTask2;
+
+public class Potion extends MagicItem {
+	
+	public Potion() {;}
+	
+	
+
+
+	@Override
+	public void apply() {
+		System.out.println("HP가 회복되었습니다.");
+	}
+
+}
