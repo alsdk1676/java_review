e7943f2d0fb24136881963d248eea053d93c69ec
alsdk1676 <alsdk6761@gmail.com>
2025-06-29 23:33:58 +0900

    java review 0629

---
+++ socket/src/chatTest/Client.java
@@ -0,0 +1,43 @@
+package chatTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String[] args) throws IOException {
+		Socket socket = null;
+		
+		PrintWriter writer = null;
+		String serverIp = "";
+		
+		try {
+			
+			socket = new Socket(serverIp, 1100); 
+			System.out.println("서버 연결 완료!");
+			
+			writer = new PrintWriter(socket.getOutputStream(), true);
+			
+			String message = "클라이언트임 😎";
+			
+			writer.println(message);
+			System.out.println("서버로 [" + message + "]라는 메세지를 보냈습니다.");
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(socket != null) {
+				socket.close();
+			}
+			if(writer != null) {
+				writer.close();
+			}
+			
+		}
+		
+	}
+
+}
---
+++ socket/src/chatTest/Server.java
@@ -0,0 +1,46 @@
+package chatTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args) throws IOException{
+		ServerSocket serverSocket = null;
+		
+		Socket socket = null;
+		
+		BufferedReader reader = null;
+		
+		try {
+			serverSocket = new ServerSocket(1100);
+			System.out.println("서버가 시작! 클라이언트 대기중,,");
+	
+			socket = serverSocket.accept();
+			System.out.println("클라이언트 연결 완료!");
+			
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			String message = reader.readLine();
+			System.out.println("클라이언트에게 받은 메세지 : " + message);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if(reader != null) {
+				reader.close();
+			}
+			if(socket != null) {
+				socket.close();
+			}
+			if(serverSocket != null) {
+				serverSocket.close();
+			}
+		}
+		
+	}
+
+}
